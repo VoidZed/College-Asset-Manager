4a6ba15e45b200c36778909dc74b1560572c82d8
Amit Verma <root.avanti@gmail.com>
2025-02-22 18:04:29 +0530

    Improve structure of login

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import Login from "./components/login"
 import './App.css'
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-    
+    <Login></Login>
     </>
   )
 }
