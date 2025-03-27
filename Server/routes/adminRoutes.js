78fdfd40cada6a49250e0aee534140f69552a950
Amit Verma <root.avanti@gmail.com>
2025-03-27 12:17:28 +0530

    Bug Fix Admin Token

--- Server/routes/adminRoutes.js
+++ Server/routes/adminRoutes.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const verifyAdmin= require('../middleware/verifyAdmin');
+const verifyToken= require('../middleware/verifyToken');
 const adminController=require('../controller/adminController')
 
 
 router.get("/getEmail",verifyAdmin,adminController.getEmail)
 router.post("/updateStatus",verifyAdmin,adminController.updateStatus)
 router.post("/deleteDynamicForm",verifyAdmin,adminController.deleteFormById)
-router.get("/getDynamicForms/:activity_name",verifyAdmin,adminController.getAllDynamicForms)
+
+router.get("/getDynamicForms/:activity_name",verifyToken,adminController.getAllDynamicForms)
 router.get("/getForms",verifyAdmin,adminController.getAllForms)
-router.get("/getForm/:slug",verifyAdmin,adminController.getFormBySlug)
+router.get("/getForm/:slug",verifyToken,adminController.getFormBySlug)
 router.post("/addForm",verifyAdmin,adminController.saveForm)
 router.post("/getUsers",verifyAdmin,adminController.getUsers)
 router.post("/email",verifyAdmin,adminController.email)
